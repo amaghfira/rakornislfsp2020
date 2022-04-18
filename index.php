8340b48baa8eaad526e6a84fff27da619530fb18
amaghfira <maghfira1197@gmail.com>
2022-04-18 16:34:40 +0800

    update lagi update lagi

--- index.php
+++ index.php
@@ -36,6 +36,26 @@
   <!--====== Style css ======-->
   <link rel="stylesheet" href="assets/css/style.css">
 
+  <!-- JQUERY -->
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+  <!-- <style>
+    .popup-overlay {
+      visibility: hidden;
+    }
+
+    .popup-content {
+      visibility: hidden;
+    }
+
+    .popup-overlay.active {
+      visibility: visible;
+    }
+
+    .popup-content.active {
+      visibility: visible;
+    }
+  </style> -->
 
 </head>
 
@@ -71,7 +91,7 @@
           <div class="col-lg-12">
             <nav class="navbar navbar-expand-lg">
               <a class="navbar-brand" href="#">
-                <img src="assets/icon/logobps.png" alt="Logo" width="300px">
+                <img src="assets/icon/logobps.png" alt="Logo">
               </a>
 
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarEight" aria-controls="navbarEight" aria-expanded="false" aria-label="Toggle navigation">
@@ -210,6 +230,35 @@
           </div> <!-- section title -->
         </div>
       </div> <!-- row -->
+      <div class="row">
+        <div class="col-xl12 col-lg-12">
+          <div class="" data-wow-duration="0.5s" data-wow-delay="0.6s">
+            <div class="about-content media-body">
+              <h4 class="about-text">1. Check In</h4>
+              <p>
+              <ul>
+                <li>- Untuk peserta luar Samarinda check in tanggal 19 April minimal pukul 14.00 WITA.</li>
+                <li>- Untuk peserta Samarida check in tanggal 20 April 2022 minimal pukul 14.00 WITA.</li>
+              </ul>
+              </p>
+              <br>
+              <h4 class="about-text">2. Pleno</h4>
+              <p class="text">Pleno di Ruang Mahakam 2.</p>
+              <br>
+              <h4 class="about-title">3. Buka Puasa</h4>
+              <p class="text">Sajian Buka Puasa akan disiapkan pada tanggal 20-21 April 2022. Takjil akan disajikan pada pukul 18.20 Wita. Makanan utama akan disajikan pada pukul 18.30 Wita.</p>
+              <br>
+              <h4 class="about-title">4. Sahur dan Sarapan</h4>
+              <p class="text">Sahur dan Sarapan disediakan di Restoran. Sajian Sahur disiapkan mulai pukul 02.00 Wita. Sajian sarapan disiapkan mulai pukul 05.00 Wita.</p>
+              <br>
+              <h4 class="about-title">5. Check Out</h4>
+              <p class="text">Seluruh peserta Check Out pada tanggal 22 April 2022, maksimal pukul 12.00 Wita.</p>
+              <br>
+              <br>
+            </div>
+          </div> <!-- single about -->
+        </div>
+      </div>
 
       <div class="row">
         <div class="col-lg-6">
@@ -230,10 +279,11 @@
               <img src="assets/icon/location.png" alt="Icon" width="30px">
             </div>
             <div class="about-content media-body">
+              <br><br>
               <h4 class="about-title">Lokasi:</h4>
               <p class="text">Harris Hotel Samarinda</p>
               <p class="text">Jl. Untung Suropati No.35, Karang Asam Ulu, Kec. Sungai Kunjang, Kota Samarinda, Kalimantan Timur 75126</p>
-              <br><br>
+              <br>
               <h4 class="about-title">Contact Person:</h4>
               <p>Ely Uswatun Kasanah</p>
               <p>+62 852-9001-0001</p>
@@ -266,18 +316,29 @@
         <div class="col-lg-4 col-sm-6 branding-3 planning-3">
           <div class="single-portfolio mt-30 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
             <div class="portfolio-text">
-              <!-- Collapsable Card Example -->
+              <!-- Pop Up Card Example -->
+              <!-- <div class="card shadow mb-4" onclick="OpenModal()"> -->
               <div class="card shadow mb-4">
                 <!-- Card Header - Accordion -->
                 <a href="#komisi1card" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="komisi1card">
-                  <h6 class="m-0 font-weight-bold text-primary">KOMISI 1 - KEPALA</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">KOMISI 1 - KEPALA</h6><br>
+                  <br>
                 </a>
-                <!-- Card Content - Collapse -->
                 <div class="collapse show" id="komisi1card">
                   <div class="card-body">
-                    This is a collapsable card example using Bootstrap's built in collapse
-                    functionality. <strong>Click on the card header</strong> to see the card body
-                    collapse and expand!
+                    <h6>Koordinator: Ir. Nur Wahid</h6>
+                    <ol>
+                      <li>Hotbel Purba</li>
+                      <li>Bayu Agung Prasetio</li>
+                      <li>Ir. Srie Sis Sugianto, M.Si</li>
+                      <li>Akhmad Junaidi, S.E, MSi</li>
+                      <li>Ir. Bahramsyah</li>
+                      <li>Achmad Yasid Wijaya</li>
+                      <li>Mustaqim</li>
+                      <li>Roosmawati</li>
+                      <li>Widiyantono</li>
+                      <br><br><br><br><br><br><br><br><br><br><br>
+                    </ol>
                   </div>
                 </div>
               </div>
@@ -287,18 +348,33 @@
         <div class="col-lg-4 col-sm-6 branding-3 planning-3">
           <div class="single-portfolio mt-30 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.2s">
             <div class="portfolio-text">
-              <!-- Collapsable Card Example -->
+              <!-- Pop Up Card Example -->
+              <!-- <div class="card shadow mb-4" onclick="OpenModal()"> -->
               <div class="card shadow mb-4">
                 <!-- Card Header - Accordion -->
-                <a href="#komisi2card" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="komisi2card">
-                  <h6 class="m-0 font-weight-bold text-primary">KOMISI 2 - BIDANG MANAGEMENT PENDATAAN</h6>
+                <a href="#komisi2card" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="komisi1card">
+                  <h6 class="m-0 font-weight-bold text-primary">KOMISI 2 - BIDANG MANAGEMENT PENDATAAN</h6><br>
                 </a>
-                <!-- Card Content - Collapse -->
                 <div class="collapse show" id="komisi2card">
                   <div class="card-body">
-                    This is a collapsable card example using Bootstrap's built in collapse
-                    functionality. <strong>Click on the card header</strong> to see the card body
-                    collapse and expand!
+                    <h6>Koordinator: Ely Uswatun Kasanah, SST, M.Ec.Dev</h6>
+                    <ol>
+                      <li>Anang Subhan Efendi, SST</li>
+                      <li>Indri Astanti, SST, M.Si</li>
+                      <li>Sunardi, SST</li>
+                      <li>Ir. Emmy Maksum, M.Si</li>
+                      <li>Sulekan, S.Sos, M.Si</li>
+                      <li>Maspupah</li>
+                      <li>MIftahul Jannah</li>
+                      <li>Triana Kurniawati, S.ST</li>
+                      <li>Hendro Budiyono, S.SI</li>
+                      <li>Sofida Kusuma Dewi</li>
+                      <li>Muryanto</li>
+                      <li>Umar Riyadi</li>
+                      <li>Edison Situmorang</li>
+                      <li>Intan Kusuma Negara</li>
+                      <br><br><br><br><br>
+                    </ol>
                   </div>
                 </div>
               </div>
@@ -311,15 +387,34 @@
               <!-- Collapsable Card Example -->
               <div class="card shadow mb-4">
                 <!-- Card Header - Accordion -->
-                <a href="#komisi3card" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="komisi3card">
+                <a href="#komisi3card" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="komisi2card">
                   <h6 class="m-0 font-weight-bold text-primary">KOMISI 3 - BIDANG KEUANGAN, LOGISTIK, DAN HUBUNGAN KELEMBAGAAN</h6>
                 </a>
                 <!-- Card Content - Collapse -->
                 <div class="collapse show" id="komisi3card">
                   <div class="card-body">
-                    This is a collapsable card example using Bootstrap's built in collapse
-                    functionality. <strong>Click on the card header</strong> to see the card body
-                    collapse and expand!
+                    <h6>Koordinator: Marinda Dama Prianto, S.Si</h6>
+                    <ol>
+                      <li>Rudy Ramadani S.,S.ST</li>
+                      <li>Mursinah, S.E, M.Si</li>
+                      <li>Lutfi Muta'ali, S.E, MM.</li>
+                      <li>Muhammad Fauzi, S.E, MM.</li>
+                      <li>Ir. Asfi Amanah, MP.</li>
+                      <li>Ardiansyah, SH, MM</li>
+                      <li>Titik Hidayati, SST, M.SE</li>
+                      <li>Akbar Darmawan, SST.</li>
+                      <li>Christin Uli Arta, S.Si</li>
+                      <li>M. Husein Labib, SH</li>
+                      <li>Maulana Malik Herdianto</li>
+                      <li>Mujiono</li>
+                      <li>Denok Purbondari</li>
+                      <li>Norhaini, S.SI</li>
+                      <li>Arina Rusyda</li>
+                      <li>Lili Riduan</li>
+                      <li>Eric Ismail Sudjito</li>
+                      <li>Melindawati</li>
+                      <li>Diah Rukmana Sari</li>
+                    </ol>
                   </div>
                 </div>
               </div>
@@ -338,9 +433,24 @@
                 <!-- Card Content - Collapse -->
                 <div class="collapse show" id="komisi4card">
                   <div class="card-body">
-                    This is a collapsable card example using Bootstrap's built in collapse
-                    functionality. <strong>Click on the card header</strong> to see the card body
-                    collapse and expand!
+                    <h6>Koordinator: Gandi Wilyanto, SST</h6>
+                    <ol>
+                      <li>Ir. Achmad Zaini, M.Si.</li>
+                      <li>Yudi Wahyudin, SST., M.Si.</li>
+                      <li>Muhammad Syahril, SST</li>
+                      <li>Haidir Maghribi, SST</li>
+                      <li>Metha Setyoaji Wedhaninggar, S.Si., M.Sc.</li>
+                      <li>Joko Affandy Alhuda, S.Si., M.Stat.</li>
+                      <li>H. M. Adrie Dirga Sagita, S.Sos., M.Si.</li>
+                      <li>Hairuddin, S.Si</li>
+                      <li>Yohanes Eka Firma Dinata</li>
+                      <li>Yudha Satia Isnanta, S.ST</li>
+                      <li>Ari Setyanto, S.ST</li>
+                      <li>Much.Yusuf</li>
+                      <li>R.Agus Setyawan</li>
+                      <li>Rangga Adhimulya</li>
+                      <li>Gita Lestari</li>
+                    </ol>
                   </div>
                 </div>
               </div>
@@ -359,9 +469,25 @@
                 <!-- Card Content - Collapse -->
                 <div class="collapse show" id="komisi5card">
                   <div class="card-body">
-                    This is a collapsable card example using Bootstrap's built in collapse
-                    functionality. <strong>Click on the card header</strong> to see the card body
-                    collapse and expand!
+                    <h6>Koordinator: Dr. Yusniar Juliana, S.Si., MIDEC.</h6>
+                    <ol>
+                      <li>Ika Ayuningtyas, SST., M.Si.</li>
+                      <li>Khumairotus Sa’adah Juriana, SST</li>
+                      <li>Ir. Lilis Suryani, M.M.</li>
+                      <li>Ucik Purnamasari, SST., M.Si.</li>
+                      <li>Bronson Manik, S.Stat.</li>
+                      <li>Kurnia Wahyu Sahfitri, SST.</li>
+                      <li>Elok Nugrahaeni Yekti Handayani</li>
+                      <li>Toto Surya Perdana</li>
+                      <li>Nugroho Dwi Prasetio, S.ST</li>
+                      <li>Rani Ruslan, S.Si</li>
+                      <li>Alvian Ferrandy</li>
+                      <li>Nurul Istiqomah</li>
+                      <li>Loveandre Danang Handriyanto</li>
+                      <li>Norlatifah</li>
+                      <li>Novana Ari Santi</li>
+                      <br>
+                    </ol>
                   </div>
                 </div>
               </div>
@@ -375,20 +501,37 @@
               <div class="card shadow mb-4">
                 <!-- Card Header - Accordion -->
                 <a href="#komisi6card" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="komisi6card">
-                  <h6 class="m-0 font-weight-bold text-primary">KOMISI 6 - BIDANG MAKO</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">KOMISI 6 - BIDANG MAKO</h6><br>
                 </a>
                 <!-- Card Content - Collapse -->
                 <div class="collapse show" id="komisi6card">
                   <div class="card-body">
-                    This is a collapsable card example using Bootstrap's built in collapse
-                    functionality. <strong>Click on the card header</strong> to see the card body
-                    collapse and expand!
+                    <h6>Koordinator: Khairil Anwar, M.Si.</h6>
+                    <ol>
+                      <li>Deja Firda Lupitasari, SST</li>
+                      <li>Yogi Purjayanto, SST</li>
+                      <li>Aditya Wisnu Anggara, S.Tr.Stat. </li>
+                      <li>Muhammad Ricky Pranata, SST</li>
+                      <li>Aldi Fauzan Akbar, SST</li>
+                      <li>Desy Indah Permata Sari, SST</li>
+                      <li>Maspupah</li>
+                      <li>MIftahul Jannah</li>
+                      <li>Triana Kurniawati, S.ST</li>
+                      <li>Hendro Budiyono, S.SI</li>
+                      <li>Sofida Kusuma Dewi</li>
+                      <li>Muryanto</li>
+                      <li>Umar Riyadi</li>
+                      <li>Edison Situmorang</li>
+                      <li>Intan Kusuma Negara</li>
+                      <br>
+                    </ol>
                   </div>
                 </div>
               </div>
             </div>
           </div> <!-- single portfolio -->
         </div>
+        
       </div> <!-- row -->
     </div> <!-- container -->
   </section>
@@ -404,20 +547,14 @@
           <div class="section-title text-center pb-20">
             <h3 class="title">Jadwal RAKORNIS Long Form SP2020</h3>
             <p class="text">Provinsi Kalimantan Timur Tahun Anggaran 2022</p>
+            <br><br>
+            <a href="Rundown.pdf">Klik untuk Download Rundown</a>
           </div> <!-- section title -->
         </div>
       </div> <!-- row -->
       <div class="row justify-content-center">
         <div class="col-lg-4 col-md-7 col-sm-9">
-         
-        </div>
-
-        <div class="col-lg-4 col-md-7 col-sm-9">
-         
-        </div>
-
-        <div class="col-lg-4 col-md-7 col-sm-9">
-          
+              
         </div>
       </div> <!-- row -->
     </div> <!-- container -->
@@ -469,7 +606,7 @@
             <div class="pricing-list">
               <ul>
                 <li><i class="lni-check-mark-circle"></i> Download template rencana aksi persiapan RAKORNIS disini.</li>
-                
+
               </ul>
             </div>
             <div class="pricing-btn rounded-buttons text-center">
@@ -531,6 +668,17 @@
 
   <!--====== Main js ======-->
   <script src="assets/js/main.js"></script>
+
+  <script>
+    function OpenModal() {
+      $(".overlay").show();
+    }
+
+    function CloseModal() {
+      $(".overlay").hide();
+    }
+  </script>
+
 </body>
 
 </html>
